6c7ea0b33358ae523ffbc11d8b501c09cb3a1999
FALCO <aude.falco@laposte.net>
2016-11-18 16:17:05 +0100

    function generate
    generate addition

--- generateDirectory.php
+++ generateDirectory.php
@@ -1,25 +1,37 @@
 <?php
 
-$dir=dirname(__FILE__); 
-$file=scandir($dir);
-/*print_r($file);	*/
+$dir=dirname(__FILE__);
 
+
+
+
+if(isset($_REQUEST['file'])){
+  $dir = $_REQUEST['file'];
+  echo $dir;
+} else {
+	$file=$_REQUEST['file'];
+
+
+}
+
+explorerFile($dir);
+function explorerFile($dir){
+  $files = array_diff(scandir($dir),array('..', '.')); // tableau des fichiers
+  foreach ($files as $key => $value) {
+    $infoFiles[]=$value.'|'.human_filesize(filesize($value)).'|'.filetype($value).'|'.date ("d/m/Y H:i ", filemtime($value)).PHP_EOL;
+  }// On liste les informations des fichiers ou dossiers dans dir
+  foreach ($infoFiles as $key => $value){
+    $arrayFiles = explode("|", $value);
+    generateHTML($arrayFiles[0],$arrayFiles[1],$arrayFiles[2],$arrayFiles[3]);
+  }//génére les balises html qui vont bien en fonction des infos des fichiers et dossiers
+}
+/*print_r($file);	*/
 
  function human_filesize($bytes, $decimals = 2) {
         $sz = 'BKMGTP';
         $factor = floor((strlen($bytes) - 1) / 3);
         return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
       }
-
-
-  foreach ($file as $key => $value) {
-  	$infoFiles[]=$value.'|'.human_filesize(filesize($value)).'|'.filetype($value).'|'.date ("d/m/Y H:i ", filemtime($value)).PHP_EOL;
-  }
-
-  foreach ($infoFiles as $key => $value){
-  	$arrayFiles = explode("|", $value);
-     generateHTML($arrayFiles[0],$arrayFiles[1],$arrayFiles[2],$arrayFiles[3]);
-  }
 
 
   function generateHTML($folderName,$size,$type,$modified){
